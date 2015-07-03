8250443d5667de191729299ab36cb9bd1e3b238c
FelipeMDuarte <felipeduarte012@gmail.com>
2015-07-02 21:10:16 -0300

    botando no package certo (?) hue

--- src/Trabalho3CG.java
+++
@@ -1,7 +0,0 @@
-
-public class Trabalho3CG {
-
-	public static void main(String[] args) {
-		System.out.println("Rodou, porra");
-	}
-}
---
+++ src/src/Trabalho3CG.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Trabalho3CG {
+
+	public static void main(String[] args) {
+		System.out.println("Rodou, porra");
+	}
+}
