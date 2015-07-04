bae57208a200fd4d7f2c866c7d25c4bdbc594261
Fernando Gouvea <fernandosrg1@gmail.com>
2015-07-03 22:08:17 -0300

    Mostrando só a imagem original e a atual.

--- src/src/Window.java
+++ src/src/Window.java
@@ -10,6 +10,8 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -28,29 +30,29 @@ import util.ArrayData;
 
 
 public class Window implements Runnable, ActionListener {  
-	JFrame frame =new JFrame();//creating instance of JFrame 
+	private JFrame frame =new JFrame();//creating instance of JFrame 
 	
-	JMenuBar menuBar = new JMenuBar();
-	JMenu fileMenu= new JMenu("File");
-	JMenu editMenu= new JMenu("Edit");
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu fileMenu= new JMenu("File");
+	private JMenu editMenu= new JMenu("Edit");
 	
-	JMenuItem openMenuItem = new JMenuItem("Open");
-	JMenuItem saveMenuItem = new JMenuItem("Save");
-	JMenuItem convoMenuItem = new JMenuItem("Convolu��o");
-	JMenuItem cropMenuItem = new JMenuItem("Crop");
-	JMenuItem sepiaMenuItem = new JMenuItem("Sepia");
+	private JMenuItem openMenuItem = new JMenuItem("Open");
+	private JMenuItem saveMenuItem = new JMenuItem("Save");
+	private JMenuItem convoMenuItem = new JMenuItem("Convolu��o");
+	private JMenuItem cropMenuItem = new JMenuItem("Crop");
+	private JMenuItem sepiaMenuItem = new JMenuItem("Sepia");
 	
-	JPanel panel = new JPanel();
+	private JPanel panel = new JPanel();
 	
-	JTextField filename = new JTextField();
-	JTextField dir = new JTextField();
+	private JTextField filename = new JTextField();
+	private JTextField dir = new JTextField();
 	
-	JLabel label;
+	private JLabel lblOriginalImage;
+	private JLabel lblCurrentImage;
 	
-	BufferedImage image;
-	BufferedImage imageCrop;
-	BufferedImage imageSepia;
-	BufferedImage imageConvo;
+	private BufferedImage image;
+	
+	private List<BufferedImage> previousImages;
 	
 	int startX = 0;
 	int startY= 0;
@@ -62,29 +64,27 @@ public class Window implements Runnable, ActionListener {
 	boolean cropped = false;
 	
 	public static void main(String[] args) throws IOException {  
-			
 			SwingUtilities.invokeLater( new Window());
 	}
 	
 	public void run(){
-
-				//JButton b=new JButton("click");//creating instance of JButton  
-				//b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-
-				prepareMenus();
-				
-				createCropMouseListener();
-				
-				panel.setSize(600,600);
-				
-				frame.setTitle("Trab 3 - Felipe e Fernando"); // goyzice
-				frame.setLayout(new BorderLayout()); // sei la n�o sei oq � melhor
-				frame.getContentPane().add(menuBar, BorderLayout.PAGE_START);
-				frame.getContentPane().add(panel, BorderLayout.CENTER);//adding button in JFrame
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	          
-				frame.setSize(600,600); //  tamanho padr�o 
-				frame.setVisible(true);//making the frame visible  
-			
+		//JButton b=new JButton("click");//creating instance of JButton  
+		//b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+		
+		previousImages = new ArrayList<BufferedImage>();
+		prepareMenus();
+		
+		createCropMouseListener();
+		
+		panel.setSize(600,600);
+		
+		frame.setTitle("Trab 3 - Felipe e Fernando"); // goyzice
+		frame.setLayout(new BorderLayout()); // sei la n�o sei oq � melhor
+		frame.getContentPane().add(menuBar, BorderLayout.PAGE_START);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);//adding button in JFrame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	          
+		frame.setSize(600,600); //  tamanho padr�o 
+		frame.setVisible(true);//making the frame visible  
 	}
 
 	private void prepareMenus() {
@@ -141,9 +141,9 @@ public class Window implements Runnable, ActionListener {
 		Convolucao convo = new Convolucao(image,filterSize ,filterIterations);
 		ArrayData[] array = convo.rodaFiltro();
 		try {
+			previousImages.add(image);
 			image = convo.writeOutputImage(array);     // desenha a imagem nova tendo o array resultante da convolu��o
 		} catch (IOException e) {
-			// TODO Auto-generated catch block// n�o tinha imagem
 			System.out.println("Exception dps de tentar escrever a image no label");
 			e.printStackTrace();
 		}
@@ -154,6 +154,7 @@ public class Window implements Runnable, ActionListener {
 		int sepiaIntensity = 30 ;
 		int sepiaDepth = 20;
 		try {
+			previousImages.add(image);
 			image = sepiaFilter.writeOutputImageSepia(sepiaIntensity, sepiaDepth);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -161,8 +162,8 @@ public class Window implements Runnable, ActionListener {
 	}
 
 	private void updateImage() {
-		JLabel imageLabel = new JLabel(new ImageIcon(image)); //adiciona a imagem em outro label , ainda n�o gosto disso mas parece unica op��o
-		panel.add(imageLabel , BorderLayout.CENTER);// botando a imagem num label. ?
+		lblCurrentImage.setIcon(new ImageIcon(image)); //adiciona a imagem em outro label , ainda n�o gosto disso mas parece unica op��o
+		//panel.add(imageLabel , BorderLayout.CENTER);// botando a imagem num label. ?
 		frame.pack(); // resize
 		frame.setVisible(true); // desenhar again pra botar a imagem
 	}
@@ -179,7 +180,6 @@ public class Window implements Runnable, ActionListener {
 		    try {
 				ImageIO.write(image , "PNG" ,saveFile); // salvar em jpg vai com cores erradas
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}  	
 		  }
@@ -205,8 +205,13 @@ public class Window implements Runnable, ActionListener {
 				image = null;
 				e.printStackTrace();
 			}
-			label = new JLabel(new ImageIcon(image)); // botando a imagem num label. ???? alguma coisa melhor pra isso ???? - tem que ter , �ocorro.
-			panel.add(label);
+			
+			lblOriginalImage = new JLabel(new ImageIcon(image)); // botando a imagem original num label. ???? alguma coisa melhor pra isso ???? - tem que ter , �ocorro.
+			panel.add(lblOriginalImage);
+			
+			lblCurrentImage = new JLabel();
+			panel.add(lblCurrentImage , BorderLayout.CENTER);// botando a imagem atual num label.
+			
 			frame.pack(); // resize
 			frame.setVisible(true); // desenhar again pra botar a imagem
 		}else {
@@ -232,14 +237,17 @@ public class Window implements Runnable, ActionListener {
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
+			if (!crop) return;
+			
 			this.endX = e.getX();
 			this.endY = e.getY();
+			
+			previousImages.add(image);
+
 			Crop cropper = new Crop();
-			imageCrop = cropper.crop(image, startX, startY, endX, endY); // pegar events do mouse
-			JLabel label2 = new JLabel(new ImageIcon(imageCrop));
-			panel.add(label2);
-			frame.pack();
-			frame.setVisible(true);
+			image = cropper.crop(image, startX, startY, endX, endY); // pegar events do mouse
+			
+			updateImage();
 			
 			cropped= true;
 			crop = false;
